4ee23cdb938097e39758b9e2d2de7e33382433d5
Gyu-chan <51047883+Gyu-chan@users.noreply.github.com>
2020-09-16 10:38:49 +0900

    코드수정

--- draw_main.py
+++ draw_main.py
@@ -1,22 +1,34 @@
 from tool import*
 
 while True :
-     menu()
-     ch=int(input("\n입력 >> "))
      draw_menu()
-     if ch == 1:
+     ch=int(input("\n입력 >> "))
+     sum=0
+     
+     if ch == 1:      
           input_data=input("파일명 : ")
-          input_op= int(input("옵션 선택 : "))
-
+          turn=['1/1_data/','2/2_data/','3/3_data/','4/4_data/','W/W_data/','R/R_data/']     
+          for i in range(0,len(turn)):
+               a=draw_data_time(input_data,turn[i])
+               print(turn[i][0]+" 스테이지의 EEG 이미지 %d개 생성완료"%a)
+               sum+=a
+          print(input_data+"의 이미지 총 %d개 생성하였습니다."%sum)
+          continue       
+               
+     if ch == 2:       
+          input_data=input("파일명 : ")  
           turn=['1/1_data/','2/2_data/','3/3_data/','4/4_data/','W/W_data/','R/R_data/']
 
-          if input_op == 1:
-               for i in range(0,len(turn)-1):
-                    a=draw_data(input_data,turn[i])
-                    print(turn[i][0]+" 스테이지의 EEG 이미지 %d개 생성완료"%a)
-                    sum+=a
-               print(input_data+"의 이미지 총 %d개 생성하였습니다."%sum)
+          for i in range(0,len(turn)):
+               b=draw_data_fft(input_data,turn[i])
+               print(turn[i][0]+" 스테이지의 FFT 이미지 %d개 생성완료"%b)
+               sum+=b
+          print(input_data+"의 이미지 총 %d개 생성하였습니다."%sum)
           continue
-     if ch == 2:
+     
+     if ch == 3:
           break
+
+          
                
+          
